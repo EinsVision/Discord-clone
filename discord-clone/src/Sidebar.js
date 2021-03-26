18bda048993daab5defd63af295fe596ef807dc6
Changdae <einsvision@gmail.com>
2021-03-26 14:00:11 +0900

    building sidebar

--- discord-clone/src/Sidebar.js
+++ discord-clone/src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Sidebar.css';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1>I am a Sidebar.</h1>
+      <div className="sidebar__top">
+        <h3>EinsVision</h3>
+        <ExpandMoreIcon />  
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon /> 
+            <h4>Text Channel</h4>
+          </div>
+
+          <AddIcon className='sidebar__addChannel' /> 
+        </div>
+      </div>
+      
     </div>
   )
 }
