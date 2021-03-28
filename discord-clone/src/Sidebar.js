7189d41f1a854b0344b03d96d582bb3ebeb76039
Changdae <einsvision@gmail.com>
2021-03-28 20:47:08 +0900

    building sidebar channels

--- discord-clone/src/Sidebar.js
+++ discord-clone/src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
@@ -12,10 +12,24 @@ import HeadsetIcon from '@material-ui/icons/Headset';
 import SettingsIcon from '@material-ui/icons/Settings';
 import { useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
-import { auth } from './firebase';
+import db, { auth } from './firebase';
 
 function Sidebar() {
   const user = useSelector(selectUser);
+  const [channels, setChannels] = useState([]);
+
+  useEffect( () => {
+    db.collection('channels').onSnapshot(snapshot => (
+      setChannels(snapshot.docs.map(doc => ({
+        id: doc.id,
+        channel: doc.data(),
+      })))
+    ))
+  }, []);
+
+  const handleAddChannel = () => {
+    const channelName = prompt("enter a new channel name");
+  }
 
   return (
     <div className='sidebar'>
@@ -31,14 +45,14 @@ function Sidebar() {
             <h4>Text Channel</h4>
           </div>
 
-          <AddIcon className='sidebar__addChannel' /> 
+          <AddIcon onClick={handleAddChannel} className='sidebar__addChannel' /> 
         </div>
 
         <div className="sidebar__channelsList">
-          <SidebarChannel />
-          <SidebarChannel />
-          <SidebarChannel />
-          <SidebarChannel />
+          {channels.map((channel) => (
+            <SidebarChannel channel={channel}/>
+          ))}
+       
         </div>
       </div>  
 
