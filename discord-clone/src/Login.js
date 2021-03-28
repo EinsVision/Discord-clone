2aa50ce6022f0e972a1fb1b3562fc32d57dcb650
Changdae <einsvision@gmail.com>
2021-03-28 18:19:04 +0900

    built login page

--- discord-clone/src/Login.js
+++ discord-clone/src/Login.js
@@ -3,15 +3,23 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
+
+  const signIn = () => {
+    
+  };
+
   return (
     <div className='login'>
       <div className="login__logo">
-        <img src="https://upload.wikimedia.org/wikipedia/ko/thumb/6/62/%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg/603px-%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg.png" alt=""/>
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/ko/thumb/6/62/%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg/603px-%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg.png" 
+          alt=""
+        />
       </div>
 
-      <Button></Button>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
-  )
+  );
 }
 
 export default Login
