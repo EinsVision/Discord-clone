2c8e77eb5cf37fdd21d2bc246847230e34a10e2e
Changdae <einsvision@gmail.com>
2021-03-28 18:01:00 +0900

    building login Page

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
   const user = useSelector(selectUser);
@@ -15,7 +16,7 @@ function App() {
           <Chat />
         </>
       ) : (
-        <h2>You need to login.</h2>
+        <Login />
       )}
       
     </div>
---
+++ discord-clone/src/Login.js
@@ -0,0 +1,17 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className='login'>
+      <div className="login__logo">
+        <img src="https://upload.wikimedia.org/wikipedia/ko/thumb/6/62/%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg/603px-%EB%94%94%EC%8A%A4%EC%BD%94%EB%93%9C_%EB%A1%9C%EA%B3%A0.svg.png" alt=""/>
+      </div>
+
+      <Button></Button>
+    </div>
+  )
+}
+
+export default Login
